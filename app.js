6d46187a7a6f687e4753fae735a2054ea6330144
Miguel <mrsepu@gmail.com>
2021-08-05 22:10:12 -0400

    basic appending

---
+++ app.js
@@ -0,0 +1,23 @@
+const draggables = document.querySelectorAll('.draggable');
+const containers = document.querySelectorAll('.container');
+
+draggables.forEach(draggable => {
+    draggable.addEventListener('dragstart', () => {
+        // console.log('drag start');
+        draggable.classList.add('dragging');
+    })
+
+    draggable.addEventListener('dragend', () => {
+        // console.log('drag end');
+        draggable.classList.remove('dragging')
+    })
+})
+
+containers.forEach(container => {
+    container.addEventListener('dragover', (e) => {
+        e.preventDefault();
+        // console.log('drag over');
+        const currentDragging = document.querySelector('.dragging');
+        container.append(currentDragging);
+    })
+})
